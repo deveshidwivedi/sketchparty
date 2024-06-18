c0a575b94ec18b69f1836bb5f97117257851c21a
Deveshi Dwivedi <deveshigurgaon@gmail.com>
2024-06-18 21:02:15 +0530

    update file to fix error

--- server/index.ts
+++ server/index.ts
@@ -1,40 +1,38 @@
 import { createServer } from "http";
-
 import express from "express";
-import next, { NextApiHandler } from "next";
-import {Server} from "socket.io";
+import next from "next";
+import { Server as SocketIOServer, Socket as SocketIOSocket } from "socket.io";
 
-const port= parseInt(process.env.PORT || "3000", 10);
+const port = parseInt(process.env.PORT || "3000", 10);
 const dev = process.env.NODE_ENV !== "production";
-const nextApp= next({ dev });
-const nextHandler= nextApp.getRequestHandler();
+const nextApp = next({ dev });
+const nextHandler = nextApp.getRequestHandler();
 
-nextApp.prepare().then(async() =>{
-    const app= express();
-    const server= createServer(app);
+nextApp.prepare().then(async () => {
+    const app = express();
+    const server = createServer(app);
 
-    const io= new Server<ClientToServerEvents, ServerToClientEvents>(server);
+    const io = new SocketIOServer(server);
 
-    app.get("/hello", async(_, res)=>{
+    app.get("/hello", async (_, res) => {
         res.send("Hello ji!");
     });
 
-    io.on("connection", (socket)=>{
+    io.on("connection", (socket: SocketIOSocket) => {
         console.log("connection");
 
-        socket.on("draw", (moves, options)=>{
+        socket.on("draw", (moves: [number, number][], options: any) => {
             console.log("drawing");
             socket.broadcast.emit("socket_draw", moves, options);
         });
 
-        socket.on("disconnect", ()=>{
+        socket.on("disconnect", () => {
             console.log("disconnected");
         });
-
     });
 
     app.all("*", (req, res) => nextHandler(req, res));
-    server.listen(port, () =>{
+    server.listen(port, () => {
         console.log(`Server is ready on ${port}`);
     });
 });
