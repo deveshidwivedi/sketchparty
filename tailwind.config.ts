84d75582b3bf4f6ed8bab9974baeb70d2b9dc4a2
Deveshi Dwivedi <deveshigurgaon@gmail.com>
2024-06-18 18:42:08 +0530

    add common and modules folder to config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,8 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./commons/**/*.{js,ts,jsx,tsx,mdx}",
+    "./modules/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
