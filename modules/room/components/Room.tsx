a64758471ac88f5232ad6799f4ecba95e03b1fac
Deveshi Dwivedi <deveshigurgaon@gmail.com>
2024-06-20 12:58:30 +0530

    add Room component

---
+++ modules/room/components/Room.tsx
@@ -0,0 +1,17 @@
+import RoomContextProvider from "../context/Room.context";
+import Canvas from "./Canvas";
+import { MousePosition } from "./MousePosition";
+import MouseRenderer from "./MouseRenderer";
+
+const Room = () => {
+     return (
+        <RoomContextProvider>
+            <div className="relative h-full w-full overflow-hidden">
+            <Canvas />
+            <MousePosition />
+            <MouseRenderer />
+            </div>
+        </RoomContextProvider>
+     );
+};
+export default Room;
