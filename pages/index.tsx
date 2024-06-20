5d2b66142d357c89d2bfaf53865c1f90da564211
Deveshi Dwivedi <deveshigurgaon@gmail.com>
2024-06-20 11:08:39 +0530

    update imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google";
-import Canvas from "@/modules/canvas/components/Canvas";
+import Canvas from "@/modules/room/components/Canvas";
 
 
 const inter = Inter({ subsets: ["latin"] });
