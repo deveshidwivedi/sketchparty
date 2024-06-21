973ddde0e7b1bd214425df02217f08a1c1ea6cc5
Deveshi Dwivedi <deveshigurgaon@gmail.com>
2024-06-22 01:39:03 +0530

    add roomAtom and hooks to manage room id state

---
+++ common/recoil/room/room.hooks.ts
@@ -0,0 +1,17 @@
+import { useRecoilValue, useRecoilState, useSetRecoilState } from "recoil";
+
+import { roomAtom } from "./room.atom";
+
+export const useRoomId = () => {
+    const {id} = useRecoilValue(roomAtom);
+    return id;
+};
+
+export const useSetRoomId = () => {
+    const setRoomId= useSetRecoilState(roomAtom);
+
+    const handleSetRoomId = (id: string) => {   
+        setRoomId({id});
+    };
+    return handleSetRoomId
+}
