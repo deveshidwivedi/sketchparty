bc647a9ebe14588d48d42ac067ec17a3b0baa460
Deveshi Dwivedi <deveshigurgaon@gmail.com>
2024-06-22 16:07:46 +0530

    refactor code

--- modules/room/context/Room.context.tsx
+++ modules/room/context/Room.context.tsx
@@ -1,4 +1,5 @@
 import { socket } from "@/common/lib/socket";
+import { useSetRoom, useSetUsers } from "@/common/recoil/room/room.hooks";
 import usersAtom, { useUserIds } from "@/common/recoil/users";
 import { MotionValue, useMotionValue } from "framer-motion";
 import { createContext, ReactChild, useEffect } from "react";
@@ -13,8 +14,8 @@ export const roomContext = createContext<{
 
 const RoomContextProvider = ({ children }: { children: ReactChild }) => {
 
-    const setUsers= useSetRecoilState(usersAtom);
-    const usersIds= useUserIds();
+     const setRoom = useSetRoom();
+     const {handleAddUser, handleRemoveUser} = useSetUsers();
     const x = useMotionValue(0);
     const y = useMotionValue(0);
 
@@ -45,3 +46,7 @@ const RoomContextProvider = ({ children }: { children: ReactChild }) => {
     );
 };
 export default RoomContextProvider;
+
+function useSetUser(): { handleAddUser: any; handleRemoveUser: any; } {
+    throw new Error("Function not implemented.");
+}
