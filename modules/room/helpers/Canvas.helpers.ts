92d222dbdf173a6b7e5e8f56562b802c9f4986aa
Deveshi Dwivedi <deveshigurgaon@gmail.com>
2024-06-23 16:33:22 +0530

    update background of canvas

--- modules/room/helpers/Canvas.helpers.ts
+++ modules/room/helpers/Canvas.helpers.ts
@@ -1,3 +1,5 @@
+import { CANVAS_SIZE } from "@/common/constants/canvasSize";
+
 export const handleMove = (
     move: Move,
     ctx: CanvasRenderingContext2D,
@@ -18,6 +20,26 @@ export const handleMove = (
     }
   };
 
+  //checks on the canvas
+  export const drawBackground = (ctx: CanvasRenderingContext2D) => {
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "#ccc";
+
+    for(let i=0;i<CANVAS_SIZE.height;i+=25){
+      ctx.beginPath();
+      ctx.moveTo(0,i);
+      ctx.lineTo(CANVAS_SIZE.width,i);
+      ctx.stroke(); 
+    }
+    for(let i=0;i<CANVAS_SIZE.width;i+=25){
+      ctx.beginPath();
+      ctx.moveTo(i,0);
+      ctx.lineTo(i, CANVAS_SIZE.height);
+      ctx.stroke(); 
+    }
+
+  };
+
   export const drawAllMoves = (
     ctx: CanvasRenderingContext2D,
     room: ClientRoom
@@ -25,6 +47,8 @@ export const handleMove = (
     const {usersMoves, movesWithoutUser, myMoves} = room;
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
   
+    drawBackground(ctx);
+
     movesWithoutUser.forEach((move)=>{
       handleMove(move,ctx);
     });
