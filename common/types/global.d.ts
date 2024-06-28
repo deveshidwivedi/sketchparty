0627e825bf7f8d07b222248f1228c5e4c741f8e7
Deveshi Dwivedi <deveshigurgaon@gmail.com>
2024-06-28 15:55:11 +0530

    add declaration for image

--- common/types/global.d.ts
+++ common/types/global.d.ts
@@ -1,6 +1,6 @@
 export declare global {
 
-    type Shape= 'line'|'circle'|'rect' ;
+    type Shape= 'line'|'circle'|'rect'| 'image' ;
     interface CtxOptions{
         lineWidth: number;
         lineColor: string;
@@ -9,7 +9,6 @@ export declare global {
     }
 
     interface Move {
-        shape: Shape;
         radius: number;
         width: number;
         height: number;
@@ -17,6 +16,7 @@ export declare global {
         options: CtxOptions;
         timestamp: number;
         eraser: boolean;
+        base64: string;
     }
 
     type Room = {usersMoves: Map<string, Move[]>;
